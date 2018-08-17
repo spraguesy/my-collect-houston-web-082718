e5095b0b9354e4f1f935d509bfcb11c958a73c3e
Andrew Sprague <andrewsprague@outlook.com>
2018-08-17 18:02:45 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,10 +3,3 @@ def my_collect(array)
     yield i
   end
 end
-
-languages = ['ruby', 'javascript', 'python', 'objective-c']
-students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-
-my_collect(languages) do |i|
-  puts i.upcase
-end
