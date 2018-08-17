07497182ab4053a74e5603fca3eece6da7998848
Andrew Sprague <andrewsprague@outlook.com>
2018-08-17 17:56:24 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -7,7 +7,12 @@ def my_collect(arr)
 end
 
 languages = ['ruby', 'javascript', 'python', 'objective-c']
+students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
 my_collect(languages) do |i|
   puts i.upcase
+end
+
+my_collect(students) do |name|
+  puts name.split(" ").first
 end
