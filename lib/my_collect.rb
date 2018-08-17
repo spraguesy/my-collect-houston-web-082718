f5dd577290157807b3e0e734ca510c8f309d5b8a
Andrew Sprague <andrewsprague@outlook.com>
2018-08-17 17:44:48 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(arr)
+  i = 0
+  my_coll = []
+  while i < arr.length do
+    yield (arr[i])
+    i += 1
+  end
+  my_coll
+end
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
 
+my_collect(collection) do |i|
+  i.upcase
+end
